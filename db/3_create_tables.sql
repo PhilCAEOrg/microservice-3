73d458284d4a9bcc51db000741ec742f449fe929
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-11-14 17:02:38 +0000

    Init model

---
+++ db/3_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 3, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table sdsd.
+--
+CREATE TABLE caeschema.sdsd (
+  sds Integer ,
+CONSTRAINT sds_PK PRIMARY KEY (sds)
+ 
+);
+
+
+
